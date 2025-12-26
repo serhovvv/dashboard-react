3668b54cadcd7c10d54986185ac9f2965b3b8a3c
serhovvv <oleksii.serh@gmail.com>
2025-12-27 01:09:06 +0200

    feat: logout modal changes

--- src/components/Sidebar/LogoutModal.jsx
+++ src/components/Sidebar/LogoutModal.jsx
@@ -1,39 +1,40 @@
-import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { logout } from "../../feautures/auth/authSlice";
+import BasicModal from "../ui/BasicModal";
 
 const LogoutModal = ({ isOpen, onClose }) => {
   const dispatch = useDispatch();
-  if (!isOpen) return null;
+
   const confirmLogout = () => {
     dispatch(logout());
     onClose();
   };
 
   return (
-    <div className="fixed inset-0 bg-black/20 z-50 flex items-center justify-center">
-      <div className="bg-white w-[90%] max-w-[500px] text-[var(--text-primary)] flex flex-col rounded-lg shadow-lg p-10 ">
-        <div className="flex flex-col">
+    <BasicModal open={isOpen} onClose={onClose}>
+      <div className="max-w-[500px] text-[var(--text-primary)] flex flex-col rounded-xl shadow-lg p-10">
+        <div>
           <h2 className="text-4xl font-semibold mb-4">Log out?</h2>
           <p className="mb-6">Are you sure you want to log out?</p>
         </div>
+
         <div className="flex justify-end gap-3">
           <button
             onClick={onClose}
-            className="px-4 py-2 bg-gray-200 text-black rounded-md w-[70%] hover:bg-gray-300
-            "
+            className="px-4 py-2 bg-gray-200 text-black rounded-md w-[70%] hover:bg-gray-300"
           >
             Cancel
           </button>
+
           <button
-            onClick={() => confirmLogout()}
+            onClick={confirmLogout}
             className="px-4 py-2 bg-green-600 text-white rounded-md w-[70%] hover:bg-green-700"
           >
             Logout
           </button>
         </div>
       </div>
-    </div>
+    </BasicModal>
   );
 };
 
